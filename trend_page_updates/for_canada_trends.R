d77c141ded29553901a2cfdc97c0ff9a2dbff675
Julia Greenberg <juliagreenberg@berkeley.edu>
2024-12-10 14:53:21 -0500

    created canada trends dataset oct 2023 - oct 2024

---
+++ trend_page_updates/for_canada_trends.R
@@ -0,0 +1,35 @@
+#===============================================================================
+# Create new trends dataset for Canadian cities only, Oct 2023 - Oct 2024
+#===============================================================================
+
+# Load packages
+#=====================================
+
+source('~/git/timathomas/functions/functions.r')
+ipak(c('tidyverse'))
+
+# Load data
+#=====================================
+
+# May 2023 - May 2024 -> filter to October 1, 2023 - April 30, 2024
+d1 <- read.csv('/Users/jpg23/data/downtownrecovery/stop_uplevelled_2023_2024/trends_may2023_may2024.csv') %>%
+  filter(date >= as.Date('2023-10-01') & date < as.Date('2024-05-01'))
+
+range(d1$date)
+head(d1)
+
+# May - November, 2024 -> filter to May 1, 2024 - October 31, 2024
+d2 <- read.csv('/Users/jpg23/UDP/downtown_recovery/trend_updates/canada/sept_nov_2024/stopuplevelled_canada_sept_nov_2024_outliers_removed.csv') %>%
+  filter(date < as.Date('2024-11-01')) %>%
+  select(c(city, date, normalized_distinct_clean, normalized_stops_clean))
+
+range(d2$date)
+head(d2)
+
+d <- rbind(d1, d2)
+head(d)
+range(d$date)
+
+write.csv(d,
+          '/Users/jpg23/UDP/downtown_recovery/trend_updates/canada/trend_canada_oct23_oct24.csv',
+          row.names = F)
